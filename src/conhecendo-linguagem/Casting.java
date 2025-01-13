ecfa630e9d859908a44ffad8f84c841034c3c1a6
Ruan Lopes <sruan507@gmail.com>
2025-01-13 18:19:39 -0300

    casting/conversão

---
+++ src/conhecendo-linguagem/Casting.java
@@ -0,0 +1,11 @@
+public class Casting {
+    public static void main(String[] args) {
+
+        double salarioAnual = 98275.67;
+        System.out.println(salarioAnual);   // 98275.67
+
+    //  int valor = salarioAnual;   // da erro
+        int valor = (int) salarioAnual;     // fazendo o casting/conversao -> transformando double em int
+        System.out.println(valor);      // 98275
+    }
+}
