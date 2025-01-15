0ad13ce99baa354b8b581804c29502f28c8ff9ea
Ruan Lopes <sruan507@gmail.com>
2025-01-15 15:26:22 -0300

    exercicio 1

---
+++ src/conhecendo-linguagem/Exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        /* Faça um programa que receba como entrada um valor que represente uma temperatura em
+        graus Celsius e imprima esse valor convertido em Kelvin e Fahrenheit
+
+        K = C + 273.15
+        F = C * 1.8 + 32
+        */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite uma temperatura em graus Celsius(°C): ");
+        double temperaturaCelsius = scanner.nextDouble();
+
+        double kelvin = temperaturaCelsius + 273.15;
+        double fahrenheit = temperaturaCelsius * 1.8 + 32;
+
+        System.out.println("A temperatura em Kelvin é: " + kelvin + "°K");
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit + "°F");
+    }
+}
