eee5c9e696c8e097ca11e86cf4ca8377ccc35b12
Ruan Lopes <sruan507@gmail.com>
2025-01-15 23:44:52 -0300

    OO - get/set por referencia

--- src/orientacaoObjetos/bancoRasmooEncapsulamento/Cliente.java
+++ src/orientacaoObjetos/bancoRasmooEncapsulamento/Cliente.java
@@ -2,9 +2,45 @@ package orientacaoObjetos.bancoRasmooEncapsulamento;
 
 public class Cliente {
     // atributos
-    String nome;
-    String cpf;
-    String profissao;
-    double salario;
+    private String nome;
+    private String cpf;
+    private String profissao;
+    private double salario;
 
+    //getters e setters
+    // nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // cpf
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // profissao
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    // salario
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
